d26c0b5dde431a94e5b76742336d51a60aeda23c
priyamwadasonnet <swayampriyajiii@gmail.com>
2024-08-13 00:18:44 +0530

    Initial Commit

---
+++ app/src/main/java/ru/orangesoftware/financisto/activity/LocationActivity.java
@@ -0,0 +1,11 @@
+package ru.orangesoftware.financisto.activity;
+
+import ru.orangesoftware.financisto.model.MyLocation;
+
+public class LocationActivity extends MyEntityActivity<MyLocation> {
+
+    public LocationActivity() {
+        super(MyLocation.class);
+    }
+
+}
